3d0fbf17c55e227a2a91f8fc666508e87c7890d8
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-20 17:50:11 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(arr)
+  if block_given?
+    i = 0 
+    while i < arr.length
+      yield arr[i]
+      i += 1 
+    end
+  
+    arr 
+  else 
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
